d71c9067be466ef8dbc3fc1a3bcefca9714e7e0c
Jonathan Merritt <j.s.merritt@gmail.com>
2014-06-18 13:09:20 +0100

    Added JodaTime as a dependency

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -7,8 +7,9 @@ object Dependencies {
   // Libraries
   // val specs2 = "org.specs2" %% "specs2" % "1.14"  // for example
   val commonsMath = "org.apache.commons" % "commons-math3" % "3.2"
+  val jodaTime    = "joda-time"          % "joda-time"     % "2.3"
 
   // Projects
   val hoofEventComparisonDependencies = Seq()
-  val pwaDependencies = Seq(commonsMath)
+  val pwaDependencies = Seq(commonsMath, jodaTime)
 }
